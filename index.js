673a54ed7a7e00d370e2e032907f6f438d8fd9ab
Takuya Miyajima <miyajima@acroquest.co.jp>
2016-05-12 01:22:23 +0900

    Jenkins対応 解析対象ファイルのパスが相対パスの場合、Jenkinsの詳細結果表示の際に参照エラーとなるため、絶対表示に変更する。

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (file) {
   process.env.JSHINT_CHECKSTYLE_FILE = process.env.HTMLHINT_CHECKSTYLE_FILE || 'htmlhint-checkstyle.xml';
   return checkstyle.reporter(file.htmlhint.messages.map(function (errMsg) {
     return {
-      file: path.relative(file.cwd, errMsg.file),
+      file: errMsg.file,
       error: {
         severity: errMsg.error.type,
         character: errMsg.error.col,
